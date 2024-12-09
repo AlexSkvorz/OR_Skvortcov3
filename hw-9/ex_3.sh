858421083022593fda5fd75da224bde4163b37ba
AlexSkvorz <alexskvorz@gmail.com>
2024-12-09 10:53:36 +0300

    add hw-9

---
+++ hw-9/ex_3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Синхронизация файлов между локальной машиной и удалённым сервером
+
+LOCAL_DIR=$1
+REMOTE_SERVER=$2
+REMOTE_DIR=$3
+EXCLUDE_FILE=$4
+
+echo "Starting file synchronization from $LOCAL_DIR to $REMOTE_SERVER:$REMOTE_DIR..."
+rsync -avz --exclude-from="$EXCLUDE_FILE" -e ssh "$LOCAL_DIR/" "$REMOTE_SERVER:$REMOTE_DIR"
